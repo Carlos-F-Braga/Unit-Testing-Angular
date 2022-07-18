111d500ed2832d1dc01d8d794fc956ae03049a28
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-07-18 13:18:41 -0300

    mockHeroService tests programmed

--- src/app/heroes.component.spec.ts
+++ src/app/heroes.component.spec.ts
@@ -33,7 +33,7 @@ describe('HeroesComponent', () => {
   describe('delete', () => {
 
     it('should remove the indicated hero from the heroes list', () => {
-      mockHeroService.deleteHero.and.returnValue(of(true))
+      mockHeroService.deleteHero.and.returnValue(of(true));
       component.heroes = HEROES;
 
       component.delete(HEROES[2]);
@@ -41,6 +41,25 @@ describe('HeroesComponent', () => {
       expect(component.heroes.length).toBe(2);
       expect(component.heroes[0].name).toEqual('SpiderDude');
       expect(component.heroes[1].name).toEqual('WonderfulGuy');
+    })
+
+    it('should call deleteHero with right parameter', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      let value = HEROES[2];
+
+      component.delete(value);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(value);
+    })
+
+      it('should have all heroes', () => {
+      component.heroes = HEROES;
+
+      let value = HEROES;
+
+      expect(component.heroes).toEqual(value);
     })
   })
 
